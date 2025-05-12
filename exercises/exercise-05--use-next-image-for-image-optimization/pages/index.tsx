8db5853937759879d65a004dc54c7f9a117ba80b
dikshitaarora <dikshitaarora35@gmail.com>
2025-05-12 13:36:39 +0530

    ex 5 and ex 6

--- exercises/exercise-05--use-next-image-for-image-optimization/pages/index.tsx
+++ exercises/exercise-05--use-next-image-for-image-optimization/pages/index.tsx
@@ -1,11 +1,23 @@
-// ✍️ import the Image component from 'next/image'
+import Image from 'next/image'
+import lazar from '../lazar.png'
 
-// ✍️ import "lazar.png" statically
+const IMG_URL =
+  'https://pbs.twimg.com/media/EnwXVKcVEAEyK5Z?format=jpg&name=medium'
 
 const Home = () => {
-  return {
-    /* ✍️ add the lazar image here */
-  }
+  return (
+    <>
+      <Image src={lazar} alt="Lazar" />
+
+      {/**
+       * Error: Invalid src prop (https://pbs.twimg.com/media/EnwXVKcVEAEyK5Z?format=jpg&name=medium) on `next/image`, hostname "pbs.twimg.com" is not configured
+       * under images in your `next.config.js`
+       *
+       * See more info: https://nextjs.org/docs/messages/next-image-unconfigured-host
+       */}
+      <Image src={IMG_URL} alt="Twitter Image" width={500} height={500} />
+    </>
+  )
 }
 
 export default Home
--- exercises/exercise-06--provide-page-specific-metadata/pages/index.tsx
+++ exercises/exercise-06--provide-page-specific-metadata/pages/index.tsx
@@ -1,10 +1,21 @@
-// ✍️ import Head from 'next/head'
+import Head from 'next/head'
 import { ChakraProvider, Heading } from '@chakra-ui/react'
 
 const Home = () => {
   return (
     <ChakraProvider>
       {/* ✍️ add title and description using the Head component */}
+      <Head>
+        <title>Homepage</title>
+        <meta name="description" content="This is my Next.js app" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Head>
+        <meta name="og:title" content="NextJS Course" key="title" />
+      </Head>
+      <Head>
+        <meta name="og:title" content="NextJS trial" key="title" />
+      </Head>
       <Heading w="full" textAlign="center">
         This is our homepage content!
       </Heading>
